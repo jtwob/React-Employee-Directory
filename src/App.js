63904043d7016d1832439a368d459ef76706d7c7
jtwob <32282322+jtwob@users.noreply.github.com>
2021-02-25 09:19:29 -0800

    NEW: added a navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./Components/Nav/Nav";
 import Table from "./Components/Table/Table";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
+        <Nav />
         <Table />
       </div>
     </div>
