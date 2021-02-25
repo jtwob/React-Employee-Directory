b968549711f7e00aa98fc5b9ab38d113579ebf36
jtwob <32282322+jtwob@users.noreply.github.com>
2021-02-25 09:31:04 -0800

    UPDATE: removed old comments

--- src/Components/Table/TableRow/TableRow.js
+++ src/Components/Table/TableRow/TableRow.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const TableRow = (props) => {
-  // console.log(props.id, props.firstName, props.lastName, props.email);
   return (
     <tr>
       <td>{props.firstName}</td>
